d4ef322e8be9282ee1959e42c686881cc2a98694
Gediontkd <gediondaniel321@gmail.com>
2024-07-09 01:34:23 -0700

    updated

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-'''Task 2's module.
-'''
-import asyncio
 import time
-
-
-wait_n = __import__('1-concurrent_coroutines').wait_n
-
+from 1-concurrent_coroutines import wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''Computes the average runtime of wait_n.
-    '''
+    """Measures the total execution time for wait_n and returns total_time / n."""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start_time) / n
+    total_time = time.time() - start_time
+    return total_time / n
+
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
 import asyncio
 from typing import List
-
-
-task_wait_random = __import__('3-tasks').task_wait_random
-
+from 3-tasks import task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """Spawns task_wait_random n times with the specified max_delay and returns the list of delays in ascending order."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
+
